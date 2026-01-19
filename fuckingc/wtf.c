d029413c97d9f7e6c6f4c704185bf398cd8c164b
ilpenSE <lastplayer8945@gmail.com>
2026-01-19 20:16:15 +0300

    Update Logger submodule - Added some C shit

---
+++ fuckingc/wtf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define a int
+#define b main
+#define c (
+#define d )
+#define e {
+#define f printf
+#define g "Hello, World!\n"
+#define h ;
+#define i }
+
+a b c d e f c g d h i
