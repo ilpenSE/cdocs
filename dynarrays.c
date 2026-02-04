cc876b11aba6fe0506a9e2b3b36cd936fd75ae4e
ilpenSE <lastplayer8945@gmail.com>
2026-02-04 18:55:36 +0300

    Added string library - String library has its tests - Changed dynamic arrays for safety reasons

--- dynarrays.c
+++ dynarrays.c
@@ -19,30 +19,32 @@ typedef struct {
 	size_t capacity;
 } Names;
 
-#define da_append(darr, item) do { \
-		if (darr.count >= darr.capacity) { \
-			if (darr.capacity == 0) darr.capacity = 32; \
-			else darr.capacity *= 2; \
-			darr.items = realloc(darr.items, darr.capacity * sizeof(*darr.items)); \
-		} \
-		darr.items[darr.count++] = item; \
-	} \
-	while (0)
+// assumes xs as NON-NULL pointer (you have to check)
+#define da_append(xs, x) do {                                           \
+    size_t new_cap = (xs)->capacity;                                    \
+    if ((xs)->count >= new_cap) {                                       \
+      new_cap = new_cap == 0 ? 64 : new_cap * 2;                        \
+      (xs)->items = realloc((xs)->items, new_cap * sizeof(*(xs)->items)); \
+    }                                                                   \
+    if ((xs)->items) {                                                  \
+      (xs)->items[(xs)->count++] = x;                                   \
+      (xs)->capacity = new_cap;                                         \
+    }                                                                   \
+  } while (0)
 
 int main() {
 	Numbers nums = {0};
-	for (int i = 0; i < 10; ++i) da_append(nums, i);
+	for (int i = 0; i < 10; ++i) da_append(&nums, i);
 	for (int i = 0; i < nums.count; ++i) printf("%d\n", nums.items[i]);
 
 	Prices prices = {0};
-	for (float i = 0.0f; i < 50.0f; i += 1.0f) da_append(prices, i);
+	for (float i = 0.0f; i < 50.0f; i += 1.0f) da_append(&prices, i);
 	for (int i = 0; i < prices.count; ++i) printf("%f\n", prices.items[i]);
 
 	Names names = {0};
-	da_append(names, "World");
-	da_append(names, "Freunde!");
-	da_append(names, "ilpeN");
+	da_append(&names, "World");
+	da_append(&names, "Freunde!");
+	da_append(&names, "ilpeN");
 	for (int i = 0; i < names.count; ++i) printf("%s\n", names.items[i]);
-
 	return 0;
 }
