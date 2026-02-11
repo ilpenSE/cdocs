0e45dac6365983f5efe9ae6babf49ece4b4dfe56
ilpenSE <lastplayer8945@gmail.com>
2026-02-11 20:10:26 +0300

    Update str.h repo

--- dynarrays.c
+++ dynarrays.c
@@ -35,16 +35,18 @@ typedef struct {
 int main() {
 	Numbers nums = {0};
 	for (int i = 0; i < 10; ++i) da_append(&nums, i);
-	for (int i = 0; i < nums.count; ++i) printf("%d\n", nums.items[i]);
+	for (size_t i = 0; i < nums.count; ++i) printf("%d\n", nums.items[i]);
 
 	Prices prices = {0};
 	for (float i = 0.0f; i < 50.0f; i += 1.0f) da_append(&prices, i);
-	for (int i = 0; i < prices.count; ++i) printf("%f\n", prices.items[i]);
+	for (size_t i = 0; i < prices.count; ++i) printf("%f\n", prices.items[i]);
 
-	Names names = {0};
+  Names names = {0};
+  da_append(&names, "Hello!");        
 	da_append(&names, "World");
 	da_append(&names, "Freunde!");
-	da_append(&names, "ilpeN");
-	for (int i = 0; i < names.count; ++i) printf("%s\n", names.items[i]);
+  da_append(&names, "ilpeN");
+
+	for (size_t i = 0; i < names.count; ++i) printf("%s\n", names.items[i]);
 	return 0;
 }
